a32e24731eef464ffbd46854c01e8ca0d2d85022
Javier <jchicano43@gmail.com>
2023-11-24 13:49:00 +0100

    v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,158 @@
-import Image from 'next/image'
-
+import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="">
+      <div id="mostrarCandado" className="flex, flex-col, justify-center, absolute,  rounded-r-lg, w-full, h-36, text-center, items-center, z-10, col-span-2 ">
+        <img src="img/candado.png" id="imgCandado" className="h-20"></img>
+        <section id="cajaCandado" className="-mt-80, flex, flex-row, justify-center">
+          <img
+            src="img/nivel 1/coin1.png"
+            id="monedaClick3"
+            alt="nivel 1/coin1.png"
+            className="h-7"
+          ></img>
+          <span id="costeCandado" className="mt-5"></span>
+        </section>
       </div>
+      <section className="displayMonedas">
+        <img src="img/nivel 1/coin1.png" alt={"coin"} />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <article id="informacionMoneda1">
+          <p>Monedas/s: </p>
+          <p>Tier: </p>
+        </article>
+      </section>
+      <section className="displayMejoras">
+        <h2>¡Mejora la produccion de monedas!</h2>
+        <aside>
+          <div className="contenedorMejoras">
+            <h1>Arma</h1>
+            <section className="informacion">
+              <p id="levelCountArma">info nv</p>
+              <div>
+                <img
+                  src="img/nivel 1/coin1.png"
+                  className="monedaPrecio"
+                  alt="nivel 1/coin1.png"
+                ></img>
+                <span id="costeArma">coste</span>
+              </div>
+            </section>
+          </div>
+          <div className="contenedorMejoras">
+            <h1>Soldado</h1>
+            <section className="informacion">
+              <p id="levelCountSoldado">info nv</p>
+              <div>
+                <img
+                  src="img/nivel 1/coin1.png"
+                  className="monedaPrecio"
+                  alt="nivel 1/coin1.png"
+                ></img>
+                <span id="costeSoldado">coste</span>
+              </div>
+            </section>
+            <div className="cuadroTexto">
+              Aumenta tu produccion de monedas por segundo en +1
+            </div>
+          </div>
+          <div className="contenedorMejoras">
+            <h1>Sargento</h1>
+            <section className="informacion">
+              <p id="levelCountSargento">info nv</p>
+              <div>
+                <img
+                  src="img/nivel 1/coin1.png"
+                  className="monedaPrecio"
+                  alt="nivel 1/coin1.png"
+                ></img>
+                <span id="costeSargento">coste</span>
+              </div>
+            </section>
+            <div className="cuadroTexto">
+              Aumenta tu produccion de monedas por segundo en +3
+            </div>
+          </div>
+          <div className="contenedorMejoras">
+            <h1>Capitán</h1>
+            <section className="informacion">
+              <p id="levelCountCapitan">info nv</p>
+              <div>
+                <img
+                  src="img/nivel 1/coin1.png"
+                  className="monedaPrecio"
+                  alt="nivel 1/coin1.png"
+                ></img>
+                <span id="costeCapitan">coste</span>
+              </div>
+            </section>
+            <div className="cuadroTexto">
+              Aumenta tu produccion de monedas por segundo en +5
+            </div>
+          </div>
+        </aside>
+      </section>
+      <section className="displayTienda">
+        <h3>Mejora la eficacia de tu ejercito en la tienda.</h3>
+        <aside>
+          <div>
+            <h1>Mejora 1</h1>
+            <section className="informacion">
+              <p id="levelCountCapitan">info nv</p>
+              <div>
+                <img
+                  src="img/nivel 1/coin1.png"
+                  className="monedaPrecio"
+                  alt="nivel 1/coin1.png"
+                ></img>
+                <span id="costeCapitan">coste</span>
+              </div>
+            </section>
+          </div>
+          <div>
+            <h1>PowerUps</h1>
+            <section className="informacion">
+              <p id="levelCountCapitan">info nv</p>
+              <div>
+                <img
+                  src="img/nivel 1/coin1.png"
+                  className="monedaPrecio"
+                  alt="nivel 1/coin1.png"
+                ></img>
+                <span id="costeCapitan">coste</span>
+              </div>
+            </section>
+          </div>
+          <div>
+            <h1>Talentos</h1>
+            <section className="informacion">
+              <p id="levelCountCapitan">info nv</p>
+              <div>
+                <img
+                  src="img/nivel 1/coin1.png"
+                  className="monedaPrecio"
+                  alt="nivel 1/coin1.png"
+                ></img>
+                <span id="costeCapitan">coste</span>
+              </div>
+            </section>
+          </div>
+          <div>
+            <h1>Reliquias</h1>
+            <section className="informacion">
+              <p id="levelCountCapitan">info nv</p>
+              <div>
+                <img
+                  src="img/nivel 1/coin1.png"
+                  className="monedaPrecio"
+                  alt="nivel 1/coin1.png"
+                ></img>
+                <span id="costeCapitan">coste</span>
+              </div>
+            </section>
+          </div>
+        </aside>
+      </section>
     </main>
-  )
+  );
 }
