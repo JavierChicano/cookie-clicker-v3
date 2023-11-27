10bc0ce68757b0e16a86be0d047a52b1a5d220d4
JChicano <jchicano43@gmail.com>
2023-11-27 17:30:18 +0100

    arreglo de dimensiones

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,27 +17,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <header className='rounded-r-md w-600 h-50'>
+      <header className='rounded-r-md w-[600px]	 h-14 border-b border-solid border-white bg-amber-500'>
         <ul className='flex list-none m-0 p-0 h-full rounded-l-md'>
-            <li className='flex flex-1 cursor-pointer h-full items-center justify-center'>
+            <li className='flex flex-1 cursor-pointer h-full items-center justify-center rounded-tl-lg'>
                 <a href="index.html" className='"no-underline'>
-                    <img src="img/coin.png" alt="Moneda"></img>
+                    <img src='../public/coin.png' alt="Moneda"></img>
                 </a>
             </li>
-            <li>
+            <li className='flex flex-1 cursor-pointer h-full items-center justify-center'>
                 <a href="index.html">
                     Olimpo
                 </a>
             </li>
-            <li>
+            <li className='flex flex-1 cursor-pointer h-full items-center justify-center'>
                 <a href="estadisticas.html">
                 Estadisticas
                 </a>
             </li>
-            <li id="displayMonedasSegundo" className=''>Monedas totales</li>
+            <li className=' flex flex-3 bg-red-700 rounded-tr-lg  text-lg items-center justify-center'>
+              Monedas totales
+            </li>
         </ul>
-        <div id="preguntas">
-            <img src="img/interrogacion.png" alt=""></img>
+        <div id="preguntas" className='h-10 w-10 ml-[620px] -mt-11 bg-red-600 rounded-full cursor-pointer'>
+            <img src="../public/interrogacion.png" alt="?"></img>
         </div>
     </header>
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -4,167 +4,162 @@ export default function Home() {
     <main className="grid grid-cols-[minmax(300px,300px)_minmax(500px,1fr)_minmax(550px,700px)] gap-y-10 relative">
       {/* <div
         id="mostrarCandado"
-        className="flex flex-col justify-center absolute  rounded-r-lg w-full h-36 text-center items-center z-10 col-span-2 "
+        className="flex flex-col justify-center absolute  rounded-r-lg w-full h-[36px] text-center items-center z-2 col-span-2 "
       >
-        <img src="img/candado.png" id="imgCandado" className="h-20"></img>
+        <img src="img/candado.png" id="imgCandado" className="h-[20px]"></img>
         <section
           id="cajaCandado"
-          className="-mt-8 flex flex-row justify-center"
+          className="mt-[-8px] flex flex-row justify-center"
         >
           <img
             src="@public/nivel 1/coin1.png"
             id="monedaClick3"
-            alt="nivel 1/coin1.png"
-            className="h-7"
+            alt="moneda"
+            className="h-[7px]"
           ></img>
-          <span id="costeCandado" className="mt-5"></span>
+          <span id="costeCandado" className="mt-[5px]"></span>
         </section>
       </div> */}
       {/* Display monedas */}
-      <section className="flex items-center bg-brown border-r border-b border-solid p-8 ">
+      <section className="flex items-center border-r border-b border-solid p-[8px] ">
         <img
           src="@public/nivel 1/coin1.png"
           alt={"coin"}
-          className="relative h-32 cursor-pointer active:scale-120"
+          className="relative h-[130px] cursor-pointer active:scale-120"
         />
 
         <article
           id="informacionMoneda1"
-          className="flex flex-col items-center justify-center bg-opacity-50 bg-black h-100 w-120 rounded-10 ml-20 text-white leading-7"
+          className="flex flex-col items-center justify-center bg-opacity-50 bg-black h-[100px] w-[120px] rounded-lg ml-[20px] text-white leading-[1.7px]"
         >
           <p className="mb-[-30px] mt-[-10px]">Monedas/s: </p>
           <p className="mb-[-30px]">Tier: </p>
         </article>
       </section>
-      <section className="displayMejoras bg-brown rounded-r-8 p-18 border-b border-solid border-seleccion">
-        <h2 className="mt-[-10px]">¡Mejora la produccion de monedas!</h2>
+      {/* Display mejoras */}
+      <section className="rounded-r-lg p-[18px] border-b border-solid bg-amber-300">
+        <h2 className="mt-[-10px] text-xl">¡Mejora la produccion de monedas!</h2>
         <aside className="flex flex-wrap mt-[-20px]">
-          <div className="contenedorMejoras relative flex flex-1 flex-col items-center bg-secundario p-8 mr-10 mt-10 rounded-10 cursor-pointer active:scale-120">
-            <h1 className="mt-0 mb-20">Arma</h1>
-            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-55">
+          {/* Primera caja */}
+          <div className="relative flex flex-1 flex-col items-center p-[8px] mr-[10px] mt-[10px] rounded-lg cursor-pointer active:scale-120">
+            <h1 className="mt-0 mb-[20px]">Arma</h1>
+            <section className="relative flex flex-col mt-[-35px] justify-center h-[55px]">
               <p id="levelCountArma">info nv</p>
-              <div className="relative bg-none flex flex-row justify-center items-center p-8">
+              <div className="relative flex flex-row justify-center items-center p-[8px]">
                 <img
                   src="@public/nivel 1/coin1.png"
-                  className="monedaPrecio w-20"
-                  alt="nivel 1/coin1.png"
+                  className="monedaPrecio w-[20px]"
+                  alt="moneda"
                 ></img>
                 <span id="costeArma mt-[-20px]">coste</span>
               </div>
             </section>
-            <div className="cuadroTexto hidden bg-opacity-70 bg-black p-10 rounded-5 z-2 mt-[-18px] text-white">
+            <div className="hidden bg-opacity-70 bg-black p-[10px] rounded-md mt-[-18px] text-white">
               Aumenta la eficiencia de tu click +1
             </div>
           </div>
-          <div className="contenedorMejoras relative flex flex-1 flex-col items-center bg-secundario p-8 mr-10 mt-10 rounded-10 cursor-pointer active:scale-120">
-            <h1>Soldado</h1>
-            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-55">
+          {/* Segunda caja */}
+          <div className="relative flex flex-1 flex-col items-center p-[8px] mr-[10px] mt-[10px] rounded-lg cursor-pointer active:scale-120">
+            <h1 className="mt-0 mb-[20px]">Soldado</h1>
+            <section className="relative flex flex-col mt-[-35px] justify-center h-[55px]">
               <p id="levelCountSoldado">info nv</p>
-              <div>
+              <div className="relative flex flex-row justify-center items-center p-[8px]">
                 <img
                   src="@public/nivel 1/coin1.png"
-                  className="monedaPrecio w-20"
-                  alt="nivel 1/coin1.png"
+                  className="monedaPrecio w-[20px]"
+                  alt="moneda"
                 ></img>
                 <span id="costeSoldado">coste</span>
               </div>
             </section>
-            <div className="cuadroTexto hidden bg-opacity-70 bg-black p-10 rounded-5 z-2 mt-[-18px] text-white">
+            <div className="hidden bg-opacity-70 bg-black p-[10px] rounded-md mt-[-18px] text-white">
               Aumenta tu produccion de monedas por segundo en +1
             </div>
           </div>
-          <div className="contenedorMejoras  relative flex flex-1 flex-col items-center bg-secundario p-8 mr-10 mt-10 rounded-10 cursor-pointer active:scale-120">
-            <h1>Sargento</h1>
-            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-55">
+          {/* Tercera caja */}
+          <div className="relative flex flex-1 flex-col items-center p-[8px] mr-[10px] mt-[10px] rounded-lg cursor-pointer active:scale-120">
+            <h1 className="mt-0 mb-[20px]">Sargento</h1>
+            <section className="relative flex flex-col mt-[-35px] justify-center h-[55px]">
               <p id="levelCountSargento">info nv</p>
-              <div>
+              <div className="relative flex flex-row justify-center items-center p-[8px]">
                 <img
                   src="@public/nivel 1/coin1.png"
-                  className="monedaPrecio w-20"
-                  alt="nivel 1/coin1.png"
+                  className="monedaPrecio w-[20px]"
+                  alt="moneda"
                 ></img>
                 <span id="costeSargento">coste</span>
               </div>
             </section>
-            <div className="cuadroTexto hidden bg-opacity-70 bg-black p-10 rounded-5 z-2 mt-[-18px] text-white">
+            <div className="hidden bg-opacity-70 bg-black p-[10px] rounded-md mt-[-18px] text-white">
               Aumenta tu produccion de monedas por segundo en +3
             </div>
           </div>
-          <div className="contenedorMejoras relative flex flex-1 flex-col items-center bg-secundario p-8 mr-10 mt-10 rounded-10 cursor-pointer active:scale-120">
-            <h1>Capitán</h1>
-            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-55">
+          {/* Cuarta caja */}
+          <div className="relative flex flex-1 flex-col items-center p-[8px] mr-[10px] mt-[10px] rounded-lg cursor-pointer active:scale-120">
+            <h1 className="mt-0 mb-[20px]">Capitán</h1>
+            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-[55px]">
               <p id="levelCountCapitan">info nv</p>
-              <div>
+              <div className="relative flex flex-row justify-center items-center p-[8px]">
                 <img
                   src="@public/nivel 1/coin1.png"
-                  className="monedaPrecio w-20"
-                  alt="nivel 1/coin1.png"
+                  className="monedaPrecio w-[20px]"
+                  alt="moneda"
                 ></img>
                 <span id="costeCapitan">coste</span>
               </div>
             </section>
-            <div className="cuadroTexto hidden bg-opacity-70 bg-black p-10 rounded-5 z-2 mt-[-18px] text-white">
+            <div className="hidden bg-opacity-70 bg-black p-[10px] rounded-md mt-[-18px] text-white">
               Aumenta tu produccion de monedas por segundo en +5
             </div>
           </div>
         </aside>
       </section>
-      <section className="displayTienda bg-brown rounded-8 ml-50 pl-10 border-b border-solid border-seleccion">
-        <h3 className="m-10 text-21">
+      {/* Display tienda */}
+      <section className="rounded-lg ml-[50px] pl-[10px] border-b border-solid">
+        <h3 className="m-[10px] text-xl">
           Mejora la eficacia de tu ejercito en la tienda.
         </h3>
-        <aside className="flex flex-wrap p-10 mt-[-20px]">
-          <div className="flex-1 flex flex-col items-center justify-center bg-secundario p-8 mr-10 mt-10 rounded-10 cursor-pointer active:scale-120">
-            <h1>Mejora 1</h1>
-            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-55">
+        <aside className="flex flex-wrap p-[10p] mt-[-20px]">
+          {/* Primera caja */}
+          <div className="flex-1 flex flex-col items-center justify-center p-[8px] mr-[10px] mt-[10px] rounded-lg cursor-pointer active:scale-120">
+            <h1 className="mt-0">PowerUps</h1>
+            <section className="relative flex flex-col mt-[-35px] justify-center h-[55px]">
               <p id="levelCountCapitan">info nv</p>
-              <div>
+              <div className="relative mt-[-20px] flex flex-row justify-center items-center">
                 <img
                   src="@public/nivel 1/coin1.png"
-                  className="monedaPrecio w-20"
-                  alt="nivel 1/coin1.png"
+                  className="monedaPrecio w-[20px]"
+                  alt="moneda"
                 ></img>
                 <span id="costeCapitan">coste</span>
               </div>
             </section>
           </div>
-          <div className="flex-1 flex flex-col items-center justify-center bg-secundario p-8 mr-10 mt-10 rounded-10 cursor-pointer active:scale-120">
-            <h1>PowerUps</h1>
-            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-55">
+          {/* Segunda caja */}
+          <div className="flex-1 flex flex-col items-center justify-center p-[8px] mr-[10px] mt-[10px] rounded-lg cursor-pointer active:scale-120">
+            <h1 className="mt-0">Talentos</h1>
+            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-[55px]">
               <p id="levelCountCapitan">info nv</p>
-              <div className="relative bg-none mt-[-20px] flex flex-row justify-center items-center">
+              <div className="relative mt-[-20px] flex flex-row justify-center items-center">
                 <img
                   src="@public/nivel 1/coin1.png"
-                  className="monedaPrecio w-20"
-                  alt="nivel 1/coin1.png"
+                  className="monedaPrecio w-[20px]"
+                  alt="moneda"
                 ></img>
                 <span id="costeCapitan">coste</span>
               </div>
             </section>
           </div>
-          <div className="flex-1 flex flex-col items-center justify-center bg-secundario p-8 mr-10 mt-10 rounded-10 cursor-pointer active:scale-120">
-            <h1>Talentos</h1>
-            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-55">
+          {/* Tercera caja */}
+          <div className="flex-1 flex flex-col items-center justify-center p-[8px] mr-[10px] mt-[10px] rounded-lg cursor-pointer active:scale-120">
+            <h1 className="mt-0">Reliquias</h1>
+            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-[55px]">
               <p id="levelCountCapitan">info nv</p>
               <div className="relative bg-none mt-[-20px] flex flex-row justify-center items-center">
                 <img
                   src="@public/nivel 1/coin1.png"
-                  className="monedaPrecio w-20"
-                  alt="nivel 1/coin1.png"
-                ></img>
-                <span id="costeCapitan">coste</span>
-              </div>
-            </section>
-          </div>
-          <div className="flex-1 flex flex-col items-center justify-center bg-secundario p-8 mr-10 mt-10 rounded-10 cursor-pointer active:scale-120">
-            <h1>Reliquias</h1>
-            <section className="informacion relative flex flex-col mt-[-35px] justify-center h-55">
-              <p id="levelCountCapitan">info nv</p>
-              <div className="relative bg-none mt-[-20px] flex flex-row justify-center items-center">
-                <img
-                  src="@public/nivel 1/coin1.png"
-                  className="monedaPrecio w-20"
-                  alt="nivel 1/coin1.png"
+                  className="monedaPrecio w-[20px]"
+                  alt="moneda"
                 ></img>
                 <span id="costeCapitan">coste</span>
               </div>
